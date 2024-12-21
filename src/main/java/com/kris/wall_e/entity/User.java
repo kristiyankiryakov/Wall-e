fd97690c9470d71885336c374224656c9e46c40c
Kris <emineo@abv.bg>
2024-12-21 12:30:39 +0200

    add user entity

--- src/main/java/com/kris/wall_e/entity/User.java
+++ src/main/java/com/kris/wall_e/entity/User.java
@@ -1,13 +1,24 @@
 package com.kris.wall_e.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 
 @Entity
 @Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
 }
