ccff72ac915230cd24c4ef0ff9f8a0f86916ed01
Kris <emineo@abv.bg>
2024-12-21 12:22:08 +0200

    initial

---
+++ src/main/java/com/kris/wall_e/WallEApplication.java
@@ -0,0 +1,13 @@
+package com.kris.wall_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallEApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kris/wall_e/entity/User.java
@@ -0,0 +1,13 @@
+package com.kris.wall_e.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+
+
+}
